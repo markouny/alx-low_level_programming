1b8cd0330dd5b35a91d100b843830321092b7b74
markouny <a_markony@hotmail.com>
2023-09-13 15:24:59 -0700

    fix name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,16 @@
+#ifndef __FUNCTION_POINTERS_H_
+#define __FUNCTION_POINTERS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/* stddef.h header defines various types and macros */
+/* size_t is unsigned integral type and is the result of the sizeof keyword */
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
