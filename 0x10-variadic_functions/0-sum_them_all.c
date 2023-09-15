490f9d7f61c13c221044efb38facbb21e16b6f1b
markouny <a_markony@hotmail.com>
2023-09-15 16:27:12 -0700

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,8 +16,8 @@ unsigned int i, sum = 0;
 
 va_start(list, n);
 if (n != 0)
-	for (i = 0; i < n; sum += va_arg(list, unsigned int), i++)
-		;
+	for (i = 0; i < n; i++)
+	sum += va_arg(list, int);
 
 va_end(list);
 
